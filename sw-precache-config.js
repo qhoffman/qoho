da48d355f996ee7f0639b81b87bac9b27a01fba6
Quay <qhoffman@gmail.com>
2016-07-31 03:06:44 +0000

    working service worker build

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -4,5 +4,15 @@ module.exports = {
     '/manifest.json',
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
   ],
+  runtimeCaching: [{
+        urlPattern: /\/bower_components\//,
+        handler: "fastest"
+    }, {
+        urlPattern: /\/src\//,
+        handler: "fastest"
+    },  {
+        urlPattern: /\/elements\//,
+        handler: "fastest"
+    }],
   navigateFallback: '/index.html'
 };
